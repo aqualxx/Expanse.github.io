1b0b5468c6f2bd12ac6598c78391a2d18f16d9cc
aqualxx <56332220+aqualxx@users.noreply.github.com>
2020-10-10 20:57:19 +1300

    reduce gravity effect

--- js/prestiges/quarks.js
+++ js/prestiges/quarks.js
@@ -56,8 +56,8 @@ function getGravityEffect() {
     let logAmount = player.achievements.includes("18") ? "4" : "3"
     let quarkup1 = player.quarks.ups.includes("1");
 
-    if (quarkup1) effect = player.quarks.gravity.max(1).log(logAmount).log10().plus(1)
-    else effect = player.quarks.gravity.max(1).log(logAmount).plus(1)
+    if (quarkup1) effect = player.quarks.gravity.max(1).log(logAmount).log10().pow("0.9").plus(1)
+    else effect = player.quarks.gravity.max(1).log(logAmount).pow("0.9").plus(1)
 
     return formatValue(effect.toFixed("2").toString(), 2)
 }
