6e3c7dba5b302af31c2811a63f0a61c8abf9e7d2
aqualxx <jetwilton@outlook.com>
2020-10-11 16:40:52 +1300

    fix nan bug

--- js/prestiges/quarks.js
+++ js/prestiges/quarks.js
@@ -64,8 +64,6 @@ function getGravityEffect() {
     let logAmount = player.achievements.includes("18") ? "4" : "3"
     let quarkup1 = player.quarks.ups.includes("2");
 
-    if (isNaN(player.quarks.gravity.max(1).log(logAmount).log10().pow("0.8"))) return
-
     if (quarkup1) effect = !isNaN(player.quarks.gravity.max(1).log(logAmount).log10().pow("0.8")) ? player.quarks.gravity.max(1).log(logAmount).log10().pow("0.8").plus(1) : "1"
     else effect = !isNaN(player.quarks.gravity.max(1).log(logAmount).pow("0.8").plus(1)) ? player.quarks.gravity.max(1).log(logAmount).pow("0.8").plus(1) : "1"
 
