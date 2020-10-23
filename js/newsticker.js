8cc7725705d6c65faabf9a6bbd69a9b76d0aea56
aqualxx <jetwilton@outlook.com>
2020-10-23 21:52:33 +1300

    News tickers :D

--- js/newsticker.js
+++ js/newsticker.js
@@ -3,6 +3,7 @@
  * You can find the orignal script here: https://github.com/IvarK/IvarK.github.io/blob/master/javascripts/core/newsticker.js
  * PS: There are spoilers for antimatter dimensions and swearing, so be warned.
  */
+
 var newsArray;
 var s = document.getElementById('news');
 document.addEventListener("visibilitychange", function () {
@@ -53,7 +54,14 @@ newsArray = [
     "No logarithm, no game.",
     "A photon travels to a space with a quark which type was a strange. He asks \"\" and realized it was a quirk!",
     "Oh, do you really think news tickers are useless? Well you are completly wrong. I can... uh... send you to different links? And... I can entertain you (at least I think I can). There, proof I am not useless.",
-    "Hey there, i'm news ticker, and heres my tip of the day: <span style=\"color:rgb(255, 50, 50);\">ERROR</span>: Could not find index 42 of [REDACTED]!"
+    "Hey there, i'm news ticker, and heres my tip of the day: <span style=\"color:rgb(255, 50, 50);\">ERROR</span>: Could not find index 42 of [REDACTED]!",
+    "Isn't spacetime supposed to be increasing in size, instead of just space?",
+    "Yes, I did just slap that <sup>3</sup> to everything including something to do with three dimensional size.",
+    "There aren't any errors in my game, it's just that your browser is acting wack.",
+    "BREAKING NEWS: Asking aqualxx to add something into the game actually works!",
+    "These are very original news tickers, according to this game we are getting data from.",
+    "Don't mind me and my 6 trillion commits to fix tiny things, it's nothing compared to my other projects.",
+    "Maybe my game breaks because I release updates too early... wait that's not in the script?"
 ];
 /*
 let script = 
